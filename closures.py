378963e1384f99b04df2c0bf5719299c494ec0ba
Enrique <octepc.97@gmail.com>
2022-10-09 12:24:20 -0500

    reviwed closures with the example

--- closures.py
+++ closures.py
@@ -9,33 +9,38 @@
 # my_func = main()
 # my_func()
 
+# def make_multiplier(x):
 
-def make_multiplier(x):
+#     def multiplier(n):
+#         return n * x
 
-    def multiplier(n):
-        return n * x
+#     return multiplier
 
-    return multiplier
+# times10 = make_multiplier(10)
+# times4 = make_multiplier(4)
+
+# print(times10(3))
+# print(times4(5))
+# print(times10(times4(2)))
 
 
-times10 = make_multiplier(10)
-times4 = make_multiplier(4)
+def make_repeater_of(n):
 
-print(times10(3))
-print(times4(5))
-print(times10(times4(2)))
+    def repeater(string):
+        assert type(string) == str, "Solo puedes usar cadenas"
+        return string * n
 
-# def make_repeater_of(n):
+    return repeater
 
-#     def repeated(string):
-#         assert type(string) == str, "solo pedes usar cadenas"
-#         return string * n
 
-#     return repeated
+def run():
+    repeater_of_10 = make_repeater_of(10)
+    repeater_of_2 = make_repeater_of(2)
 
-# def run():
-#     repeat_5 = make_repeater_of(5)
-#     print(repeat_5('Hola'))
+    print(repeater_of_10('Sucaritas'))
+    print(repeater_of_2('Hola '))
+    # print(repeater_of_10(2))
 
-# if __name__ == '__main__':
-#     run()
+
+if __name__ == '__main__':
+    run()
